11722b9cd21bcd77bb46ad6c3f604d8640ddd523
rahul pareek <itsrahul615@gmail.com>
2025-08-03 15:38:37 +0530

    learned and applied contional location selector

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <>
-      <Fetch/>
-      {/* <Location/> */}
+      {/* <Fetch/> */}
+      <Location/>
     </>
   )
 }
--- myapp/src/Location.jsx
+++ myapp/src/Location.jsx
@@ -32,80 +32,75 @@ const data = {
 };
 
 function Location() {
-  const [selectedCountry, setSelectedCountry] = useState("");
-  const [selectedState, setSelectedState] = useState("");
-  const [selectedCity, setSelectedCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
 
   const handleCountryChange = (e) => {
-    setSelectedCountry(e.target.value);
-    setSelectedState("");
-    setSelectedCity("");
-  };
-
-  const handleStateChange = (e) => {
-    setSelectedState(e.target.value);
-    setSelectedCity("");
-  };
-
-  const handleCityChange = (e) => {
-    setSelectedCity(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    console.log("Country:", selectedCountry);
-    console.log("State:", selectedState);
-    console.log("City:", selectedCity);
-  };
+    setCountry(e.target.value);
+    setState("");
+    setCity("");
+  }
+  const handleStateChange = (e) =>{
+    setState(e.target.value);
+    setCity("");
+  }
+  const handleCityChange = (e) =>{
+    setCity(e.target.value);
+  }
 
   const countries = Object.keys(data);
-  console.log(countries);
-  console.log(data);
-  const states =selectedCountry && data[selectedCountry] ? Object.keys(data[selectedCountry].states) : [];
-  const cities =selectedCountry && selectedState ? data[selectedCountry].states[selectedState] : [];
+
+  let statess = [];
+  if (country) {
+    statess = Object.keys(data[country].states);
+  }
+
+  let cities=[];
+  if(country && state){
+    cities = data[country].states[state]
+  }
+
+  const handleSubmit = () => {
+    if(!(country && state && city)){
+      alert("select all");
+      return
+    }
+
+    const data = {country,state,city};
+    console.log(data);
+  }
 
   return (
     <div>
       <h2>Select Country, State, and City</h2>
-
-      <label>Country:</label>
-      <select value={selectedCountry} onChange={handleCountryChange}>
-        <option value="">-- Select Country --</option>
-        {countries.map((country) => (
-          <option key={country} value={country}>
-            {country}
-          </option>
+      <label>Select: </label>
+      <select value={country} onChange={handleCountryChange}>
+        <option value="">Select Country</option>
+        {countries.map((cy) => (
+          <option value={cy}>{cy}</option>
         ))}
       </select>
-
       <br /><br />
-
-      <label>State:</label>
-      <select value={selectedState} onChange={handleStateChange}>
-        <option value="">-- Select State --</option>
-        {states.map((state) => (
-          <option key={state} value={state}>
-            {state}
-          </option>
+      <label>Select State </label>
+      <select value={state} onChange={handleStateChange}>
+        <option value="">Select State</option>
+        {statess.map((st) =>(
+          <option value={st}>{st}</option>
         ))}
       </select>
-
       <br /><br />
-
-      <label>City:</label>
-      <select value={selectedCity} onChange={handleCityChange}>
-        <option value="">-- Select City --</option>
-        {cities.map((city) => (
-          <option key={city} value={city}>
-            {city}
-          </option>
+      <label>Select City </label>
+      <select value={city} onChange={handleCityChange}>
+        <option value="">Select City</option>
+        {cities.map((cit) =>(
+          <option value={cit}>{cit}</option>
         ))}
       </select>
-
-      <br /><br />
-
-      <button onClick={handleSubmit}>Print to Console</button>
+      <br /> <br />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
-  );
+  )
 }
 
 export default Location;
